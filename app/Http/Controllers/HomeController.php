491e482951178bcc4a604e0b2b6213f00c0b3ae2
ArmandoFMM <armando.f.monjane@gmail.com>
2017-06-23 17:32:55 +0200

    Fix home Route

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Area;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -13,7 +14,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth')->only(['index']);
     }
 
     /**
@@ -24,5 +25,11 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    public function home() {
+
+        $areas = Area::all();
+        return view('welcome', compact('areas'));
     }
 }
