57c8926b7149d2794985f4bad84f35acdd76635f
Mario Macuacua <mariocarlitosmacuacua@gmail.com>
2017-06-24 13:08:57 +0200

    update admi espaco de inovaco

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -54,7 +54,7 @@ class UserController extends Controller
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\Responsegit
      */
     public function edit($id)
     {
